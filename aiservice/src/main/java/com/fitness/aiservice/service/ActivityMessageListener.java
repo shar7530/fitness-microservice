899f7a4d68dc1a4084adf8bdb29d5a30439b5fb4
Shreya <shar7530@mylaurier.ca>
2025-06-30 18:23:16 -0400

    Commited AI Service

--- aiservice/src/main/java/com/fitness/aiservice/service/ActivityMessageListener.java
+++ aiservice/src/main/java/com/fitness/aiservice/service/ActivityMessageListener.java
@@ -1,6 +1,8 @@
 package com.fitness.aiservice.service;
 
 import com.fitness.aiservice.model.Activity;
+import com.fitness.aiservice.model.Recommendation;
+import com.fitness.aiservice.repository.RecommendationRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
@@ -11,10 +13,16 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class ActivityMessageListener {
 
+     private final ActivityAiService activityAiService;
+
+     private final RecommendationRepository recommendationRepository;
+
 
      @RabbitListener(queues = "activity.queue")
-     public void processActivity(Activity activity){
+     public void processActivity(Activity activity) {
           log.info("Received Activity for processing: {}", activity.getId());
-
+//          log.info("Generated Recommendation: {}", activityAiService.generateRecommendation(activity));
+          Recommendation recommendation = activityAiService.generateRecommendation(activity);
+          recommendationRepository.save(recommendation);
      }
 }
